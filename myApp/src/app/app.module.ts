a2baa955a409ad8df8b5b7d41173947d85be6969
h.cai <caihech@hotmail.com>
2018-05-17 10:57:18 +0800

    REV header

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,18 +1,33 @@
+// Angular 模块类描述应用的部件是如何组合在一起的。 每个应用都至少有一个 Angular 模块，也就是根模块，
+// 用来引导并运行应用。 你可以为它取任何名字。常规名字是 AppModule。 也就是 app.module.ts 文件
+
+
+/*引入组件*/
+
+/*BrowserModule，浏览器解析的模块*/
 import { BrowserModule } from '@angular/platform-browser';
+/*angualrjs 核心模块*/
 import { NgModule } from '@angular/core';
-
+/*根组件*/
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 
+
+/*@NgModule 装饰器将 AppModule 标记为 Angular 模块类(也叫 NgModule 类)。
+@NgModule 接受一个元数据对象，告诉 Angular 如何编译和启动应用。*/
 @NgModule({
+   /*引入当前项目运行的的组件*/
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
+   /*引入当前模块运行依赖的其他模块*/
   imports: [
     BrowserModule
   ],
+  /*定义的服务 回头放在这个里面*/
   providers: [],
+  /*指定应用的主视图(称为根组件) 通过引导根 AppModule 来启动应用 ，这里一般写的是根组件*/
   bootstrap: [AppComponent]
 })
+
+/*根模块不需要导出任何东西， 因为其它组件不需要导入根模块。 但是一定要写*/
 export class AppModule { }
